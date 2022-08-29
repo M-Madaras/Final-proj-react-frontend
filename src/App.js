8f78f8f9bc5d2041ba73fe175740b930de6ff317
“Mason <masonmadaras@gmail.com>
2022-08-29 11:43:23 -0400

    blank react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import AddGoal from './components/AddGoal';
+import GoalList from './components/GoalsList';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <>
+    <h1>Goals</h1>
     </>
   );
 }
