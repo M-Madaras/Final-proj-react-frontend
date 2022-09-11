40ef37d06d9d41c3605fbe2a8efb64648fcd1456
“Mason <masonmadaras@gmail.com>
2022-09-10 20:46:43 -0400

    getting login in

--- src/components/Main/GoalsList.jsx
+++ src/components/Main/GoalsList.jsx
@@ -2,48 +2,6 @@ import { useEffect, useState, React } from 'react';
 import { List, Alert, Button } from 'antd';
 import GoalsCard from './GoalsCard';
 
-
-function itemAction(action, item, setLoading, setGoalListItems) {
-  setLoading(true)
-  const API_URL = `https://mtm-final-proj.web.app/goals/${item.userId}/${item.id}`
-  const params = (action === 'done')
-    ? {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ done: !item.done })
-    }
-    : {
-      method: 'DELETE'
-    }
-  fetch(API_URL, params)
-  .then(res => res.json())
-  .then(data => {
-    setGoalListItems(data)
-    setLoading(false)
-  })
-  .catch(err => {
-    console.log('error updating item: ', err)
-    setLoading(false)
-  })
-}
-
-function ListItem({ item, setLoading, setGoalListItems }) {
-  const thisClassName = item.done ? 'done' : ''
-  return (
-    <List.Item
-      actions={[
-        <Button primary key="list-done" onClick={() => itemAction('done', item, setLoading, setGoalListItems)}>done</Button>,
-        <Button danger key="list-delete" onClick={() => itemAction('delete', item, setLoading, setGoalListItems)}>delete</Button>,
-      ]}
-      key={item.item}
-      className={thisClassName}>
-      {item.item} </List.Item>
-    )
-}
-
-
 export default function GoalList({  goallist, setGoalList, token }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
